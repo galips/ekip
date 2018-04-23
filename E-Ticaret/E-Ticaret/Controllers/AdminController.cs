8c1dc142acac54ba21b29e504aaaf590b9b86362
Mert Tekin <merttekin94@gmail.com>
2018-04-23 21:37:51 +0300

    Admin ve E-Ticaret temasi projeye eklendi.

---
+++ E-Ticaret/E-Ticaret/Controllers/AdminController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Ticaret.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Stats()
+        {
+            return View();
+        }
+
+        public ActionResult RegularForms()
+        {
+            return View();
+        }
+
+        public ActionResult ExtendedForms()
+        {
+            return View();
+        }
+
+        public ActionResult ValidationForms()
+        {
+            return View();
+        }
+
+        public ActionResult Wizard()
+        {
+            return View();
+        }
+
+        public ActionResult RegularTables()
+        {
+            return View();
+        }
+
+        public ActionResult ExtendedTables()
+        {
+            return View();
+        }
+
+        public ActionResult BootstrapTables()
+        {
+            return View();
+        }
+
+        public ActionResult DataTables()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        public ActionResult TimelinePage()
+        {
+            return View();
+        }
+
+        public ActionResult UserPage()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
